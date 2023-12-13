a3f424c8d275c7368735dea241866cc9cdd0b233
formera <95155487+deerformera@users.noreply.github.com>
2023-12-14 05:35:43 +0700

    Add files via upload

--- configuration.nix
+++ configuration.nix
@@ -1,95 +1,77 @@
 # Edit this configuration file to define what should be installed on
 # your system.  Help is available in the configuration.nix(5) man page
-# and in the NixOS manual (accessible by running ‘nixos-help’).
+# and in the NixOS manual (accessible by running `nixos-help`).
 
 { config, pkgs, ... }:
 
 {
-  imports = [ ./hardware-configuration.nix ];
+  imports =
+    [ # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+    ];
 
   nixpkgs.config.allowUnfree = true;
 
-  environment.systemPackages = with pkgs; [
-    neovim
-    pfetch
-    wget
-    kitty alacritty
-    home-manager
-    brave firefox
-    gparted
-    btop
-    git
-    wineWowPackages.stable
-    winetricks
-    lutris
-    vscodium
-    pciutils
-    vlc
-    unzip
-  ];
 
-  boot.loader.efi.canTouchEfiVariables = true;
-  boot.loader.grub = {
-		enable = true;
-		version = 2;
-		efiSupport = true;
-		device = "nodev";
-		useOSProber = true;
+  boot.loader = {
+    grub = {
+      enable = true;
+      efiSupport = true;
+      device = "nodev";
+    };
+    efi = {
+      canTouchEfiVariables = true;
+      efiSysMountPoint = "/boot/efi";
+    };
   };
-  boot.kernelPackages = pkgs.linuxPackages_latest;
-
-  # security.sudo.wheelNeedsPassword = false;
 
   networking.hostName = "nixue";
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
   networking.networkmanager.enable = true;  # Easiest to use and most distros use this by default.
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
 
   time.timeZone = "Asia/Jakarta";
+
+  i18n.defaultLocale = "en_US.UTF-8";
+
   services.xserver = {
     enable = true;
-    displayManager.gdm.enable = true;
-    # desktopManager.gnome.enable = true;
-    desktopManager.xfce.enable = true;
+    layout = "us";
+    displayManager.sddm.enable = true;
+    desktopManager.plasma5.enable = true;
     # videoDrivers = [ "amdgpu" ];
   };
 
-  # services.httpd = {
-  #   enable = true;
-  #   enablePHP = true;
-  # };
+  # Enable sound.
+  # sound.enable = true;
+  # hardware.pulseaudio.enable = true;
 
-  # Configure keymap in X11
-  services.xserver.layout = "us";
-  # services.printing.enable = true;
+  users.users.rin = {
+    isNormalUser = true;
+    extraGroups = [ "wheel" ];
+  };
 
-  sound.enable = true;
-  hardware.pulseaudio.enable = true;
+  environment.systemPackages = with pkgs; [
+    # sys
+    neofetch
+    neovim
+    pfetch
+    wget
+    git
+    pciutils
 
-  # services.xserver.libinput.enable = true;
+    # apps
+    inkscape
+    gimp
+    steam-run
+    blender
+    steam
+    firefox
+  ];
 
-  # Some programs need SUID wrappers, can be configured further or are started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
+  environment.shellAliases = {
+    nixos-switch = "sudo nixos-rebuild switch";
+    nixos-config = "sudo nvim /etc/nixos/configuration.nix";
+  };
 
-  # services.openssh.enable = true;
-
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
-
-  # Copy the NixOS configuration file and link it from the resulting system
-  # (/run/current-system/configuration.nix). This is useful in case you
-  # accidentally delete configuration.nix.
-  # system.copySystemConfiguration = true;
-
-  system.stateVersion = "22.11"; # Did you read the comment?
-
+  system.stateVersion = "23.05"; # Did you read the comment?
 }
 
